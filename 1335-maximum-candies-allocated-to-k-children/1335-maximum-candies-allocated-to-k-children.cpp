b60e774d0091f20ff33f84253348e953e427fd00
imankit1 <127870987+imankit1@users.noreply.github.com>
2025-03-14 15:32:59 +0530

    Time: 27 ms (60.91%), Space: 88.2 MB (41.19%) - LeetHub

--- 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.cpp
+++ 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int maximumCandies(vector<int>& candies, long long k) {
-        // int totalCandies=0;
+        long long totalCandies=0;
         int maxCand=0;
         for(int i=0;i<candies.size();i++){
-            // totalCandies+=candies[i];
+            totalCandies+=candies[i];
             maxCand=max(maxCand, candies[i]);
         }
-        // if(totalCandies<k) return 0;
+        if(totalCandies<k) return 0;
         int s=1, e=maxCand;
         int ans=0;
         while(s<=e){
