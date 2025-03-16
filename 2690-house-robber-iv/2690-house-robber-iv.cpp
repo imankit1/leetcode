335726c96931403c706df603a948f6200e79e6b0
imankit1 <127870987+imankit1@users.noreply.github.com>
2025-03-16 12:17:54 +0530

    Time: 27 ms (47.02%), Space: 60.9 MB (39.13%) - LeetHub

--- 2690-house-robber-iv/2690-house-robber-iv.cpp
+++ 2690-house-robber-iv/2690-house-robber-iv.cpp
@@ -1,35 +1,31 @@
 class Solution {
 public:
-    bool isPossible(vector<int>& nums, int mid, int k) {
-        int house = 0;
-        for(int i = 0; i < nums.size(); i++) {
-            if(nums[i] <= mid) {
+
+    bool ispossible(vector<int>& ans, int mid, int k){
+        int house=0;
+        for(int i=0;i<ans.size();i++){
+            if(ans[i]<=mid){
                 house++;
-                i++;  
+                i++;
             }
         }
-
-        return house >= k;  
+        if(house>=k) return true;
+        return false;
     }
     int minCapability(vector<int>& nums, int k) {
-        int n = nums.size();
 
-        int l = *min_element(begin(nums), end(nums));
-        int r = *max_element(begin(nums), end(nums));
+        int s=*min_element(nums.begin(), nums.end());
+        int e=*max_element(nums.begin(), nums.end());
+        int ans=0;
+        while(s<=e){
+            int mid=s+(e-s)/2;
 
-        int result = r;
-
-        while(l <= r) { 
-            int mid = l + (r-l)/2; 
-
-            if(isPossible(nums, mid, k) == true) {
-                result = mid;
-                r = mid-1;
-            } else {
-                l = mid+1;
+            if(ispossible(nums, mid, k)){
+                ans=mid;
+                e=mid-1;
             }
+            else s=mid+1;
         }
-
-        return result;
+        return ans;
     }
 };
