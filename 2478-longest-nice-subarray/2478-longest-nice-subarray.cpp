3a5cb7e1bf53ae61deb81828dfdcbcf641ef878b
imankit1 <127870987+imankit1@users.noreply.github.com>
2025-03-18 16:42:56 +0530

    Update 2478-longest-nice-subarray.cpp

--- 2478-longest-nice-subarray/2478-longest-nice-subarray.cpp
+++ 2478-longest-nice-subarray/2478-longest-nice-subarray.cpp
@@ -16,3 +16,23 @@ public:
         return maxii;
     }
 };
+
+class Solution {
+public:
+    int longestNiceSubarray(vector<int>& nums) {
+        int l=0, r=0;
+        int x=0;
+        int maxii=1;
+        int mask=0;
+        while(r<nums.size()){
+            while((mask&nums[r])!=0){
+                mask=(mask^nums[l]);
+                l++;
+            }
+            maxii=max(maxii, r-l+1);
+            mask=(mask|nums[r]);
+            r++;
+        }
+        return maxii;
+    }
+};
