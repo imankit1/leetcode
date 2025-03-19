f9ad850365082a2d8c4d459b4a79c7ed7170c2f7
imankit1 <127870987+imankit1@users.noreply.github.com>
2025-03-19 23:18:32 +0530

    Time: 9 ms (40.49%), Space: 140 MB (47.55%) - LeetHub

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums) {
-        deque<int> flipQueue;
-        int count = 0;
-
-        for (int i = 0; i < nums.size(); i++) {
-            while (!flipQueue.empty() && i > flipQueue.front() + 2) {
-                flipQueue.pop_front();
-            }
-
-            if ((nums[i] + flipQueue.size()) % 2 == 0) {
-                if (i + 2 >= nums.size()) {
-                    return -1;
-                }
-                count++;
-                flipQueue.push_back(i);
+        int n=nums.size();
+        int cnt=0;
+        for(int i=0;i<(n-2);i++){
+            if(nums[i]==0){
+                nums[i]=1^nums[i];
+                nums[i+1]=1^nums[i+1];
+                nums[i+2]=1^nums[i+2];
+                cnt++;
             }
         }
-
-        return count;
+        if(nums[n-1]==0 || nums[n-2]==0) return -1;
+        return cnt;
     }
 };
