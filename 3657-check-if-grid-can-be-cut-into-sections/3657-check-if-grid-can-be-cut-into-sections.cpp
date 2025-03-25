858c504c3327fb18328f584a6b2b4ca0657c4ec3
imankit1 <127870987+imankit1@users.noreply.github.com>
2025-03-25 17:33:15 +0530

    Time: 99 ms (81.4%), Space: 215.6 MB (66.08%) - LeetHub

--- 3657-check-if-grid-can-be-cut-into-sections/3657-check-if-grid-can-be-cut-into-sections.cpp
+++ 3657-check-if-grid-can-be-cut-into-sections/3657-check-if-grid-can-be-cut-into-sections.cpp
@@ -1,32 +1,42 @@
 class Solution {
 public:
+    bool checkValidCuts(int n, vector<vector<int>>& rectangles) {
+        vector<pair<int, int>>x_axis, y_axis;
+        for(int i=0;i<rectangles.size();i++){
+            x_axis.push_back({rectangles[i][0], rectangles[i][2]});
+            y_axis.push_back({rectangles[i][1], rectangles[i][3]});
+        }
 
-vector<vector<int>>merge(vector<vector<int>>& rectangles){
-     sort(rectangles.begin(), rectangles.end());
+        sort(x_axis.begin(), x_axis.end());
+         
+          sort(y_axis.begin(), y_axis.end());
+        //    [](const int &a, const int& b){
+        //     return a[0]<b[0];
+        // });
 
-        vector<vector<int>>result;
-        result.push_back(rectangles[0]);
-        for(int i=1;i<rectangles.size();i++){
-            if(rectangles[i][0]<result.back()[1]){
-                result.back()[1]=max(result.back()[1], rectangles[i][1]);
+        vector<vector<int>>result_x, result_y;
+        result_x.push_back({x_axis[0].first, x_axis[0].second});
+        for(int i=1;i<x_axis.size();i++){
+            if(x_axis[i].first<result_x.back()[1]){
+                result_x.back()[0]=min(x_axis[i].first, result_x.back()[0]);
+                result_x.back()[1]=max(x_axis[i].second, result_x.back()[1]);
             }
             else{
-                result.push_back(rectangles[i]);
+                result_x.push_back({x_axis[i].first, x_axis[i].second});
             }
         }
-    return result;
-}
-    bool checkValidCuts(int n, vector<vector<int>>& rectangles) {
-       vector<vector<int>>x, y;
-       for(auto &i :rectangles){
-        x.push_back({i[0], i[2]});
-        y.push_back({i[1], i[3]});
-       }
 
-       vector<vector<int>>result1=merge(x);
-       vector<vector<int>>result2=merge(y);
-
-       if(result1.size()>=3 || result2.size()>=3) return true;
-       return false;
+        result_y.push_back({y_axis[0].first, y_axis[0].second});
+        for(int i=1;i<y_axis.size();i++){
+            if(y_axis[i].first<result_y.back()[1]){
+                result_y.back()[0]=min(y_axis[i].first, result_y.back()[0]);
+                result_y.back()[1]=max(y_axis[i].second, result_y.back()[1]);
+            }
+            else{
+                result_y.push_back({y_axis[i].first, y_axis[i].second});
+            }
+        }
+        if(result_x.size()>=3 || result_y.size()>=3) return true;
+        return false;
     }
 };
