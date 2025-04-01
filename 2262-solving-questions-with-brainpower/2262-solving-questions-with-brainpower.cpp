ea71b96c4631f25a1f9580d7152c6d694cec4c1b
imankit1 <127870987+imankit1@users.noreply.github.com>
2025-04-01 23:11:06 +0530

    Time: 2 ms (82.22%), Space: 119.7 MB (86.16%) - LeetHub

--- 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
@@ -1,19 +1,19 @@
+typedef long long ll;
 class Solution {
 public:
-
-    long long help(int idx, vector<vector<int>>& q, vector<long long>&dp){
-        //base case
-        if(idx>=q.size()){
-            return 0;
+    long long mostPoints(vector<vector<int>>& q) {
+        int n=q.size();
+        vector<ll>dp(n+1, 0);
+        for(int i=n-1;i>=0;i--){
+            ll take=q[i][0];
+            if(i+q[i][1]+1<n){
+                take+=dp[i+q[i][1]+1];
+            }
+            
+            ll not_taken=dp[i+1];
+            dp[i]=max(not_taken, take);
+            
         }
-        if(dp[idx]!=-1) return dp[idx];
-        long long  taken=q[idx][0]+help(idx+q[idx][1]+1, q, dp);
-        long long not_taken=help(idx+1, q, dp);
-        return dp[idx]=max(taken, not_taken);
-    }
-    long long mostPoints(vector<vector<int>>& questions) {
-        int n=questions.size();
-        vector<long long>dp(n+1, -1);
-         return  help(0, questions, dp);
+        return dp[0];
     }
 };
