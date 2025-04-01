66388d38c7de388598dbc8cf57a656486ada935e
imankit1 <127870987+imankit1@users.noreply.github.com>
2025-04-01 23:13:10 +0530

    Update 2262-solving-questions-with-brainpower.cpp

--- 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
@@ -1,3 +1,4 @@
+//  using tabulation method
 typedef long long ll;
 class Solution {
 public:
@@ -17,3 +18,26 @@ public:
         return dp[0];
     }
 };
+
+//using recursion and memoization
+
+class Solution {
+public:
+
+    long long help(int idx, vector<vector<int>>& q, vector<long long>&dp){
+        //base case
+        if(idx>=q.size()){
+            return 0;
+        }
+        if(dp[idx]!=-1) return dp[idx];
+        long long  taken=q[idx][0]+help(idx+q[idx][1]+1, q, dp);
+        long long not_taken=help(idx+1, q, dp);
+        return dp[idx]=max(taken, not_taken);
+    }
+    long long mostPoints(vector<vector<int>>& questions) {
+        int n=questions.size();
+        vector<long long>dp(n+1, -1);
+         return  help(0, questions, dp);
+    }
+};
+
