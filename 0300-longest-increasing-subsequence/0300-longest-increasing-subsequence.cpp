24d05c25688335cf792cea882f53d33cf55711f8
imankit1 <127870987+imankit1@users.noreply.github.com>
2025-04-07 00:02:06 +0530

    Update 0300-longest-increasing-subsequence.cpp

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,3 +1,32 @@
+
+// recursive method
+class Solution {
+public:
+    void check(vector<int>& nums, vector<int>& temp, int prev, int& maxii, int i){
+        if(i>=nums.size()){
+            if(maxii<temp.size()){
+                maxii=temp.size();
+            }
+                return;
+        }
+
+            if(prev<nums[i]){
+                temp.push_back(nums[i]);
+                check(nums, temp, nums[i], maxii, i+1);
+                temp.pop_back();
+            }
+            check(nums, temp, prev, maxii, i+1);
+        }
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int>temp;
+        int prev=INT_MIN;
+        int maxii=1;
+        check(nums, temp, prev, maxii, 0);
+        return maxii;
+    }
+};
+//  tabulation method
+
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
