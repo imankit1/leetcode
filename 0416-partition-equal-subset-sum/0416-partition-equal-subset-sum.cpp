1160925c2a23af1fe209c924bb24444b8df8d67c
imankit1 <127870987+imankit1@users.noreply.github.com>
2025-04-07 12:12:05 +0530

    Update 0416-partition-equal-subset-sum.cpp

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,3 +1,34 @@
+// recursive solution
+class Solution {
+public:
+
+bool  help(vector<int>&nums , int i,   int target){
+    if(i>=nums.size()){
+        if(target==0) return true;
+         return false;
+     }
+     if(target<0) return false;
+     
+      bool take=help(nums,   i+1, target-nums[i]);
+      bool not_take= help(nums,   i+1, target);
+
+      
+      return  take or not_take;
+}
+      bool canPartition(vector<int>& nums) {
+        int sumii=accumulate(nums.begin(), nums.end(),0);
+        if(sumii%2==1) return false;
+        int target=sumii/2;
+       
+        int i=0;
+
+         
+         return help(nums,  i,   target);  
+    }
+};
+
+// memoization 
+
 class Solution {
 public:
 
@@ -12,9 +43,22 @@ bool  help(vector<int>&nums , int i,   int target, vector<vector<int>>&dp){
       bool not_take= help(nums,   i+1, target,  dp);
 
       
-      return dp[i][target]=take || not_take;
+      return dp[i][target]=take or not_take;
 }
       bool canPartition(vector<int>& nums) {
+        int sumii=accumulate(nums.begin(), nums.end(),0);
+        if(sumii%2==1) return false;
+        int target=sumii/2;
+       
+        int i=0;
+
+        vector<vector<int>>dp(nums.size(), vector<int>(target+1, -1));
+         return help(nums,  i,   target, dp);  
+    }
+};
+
+// dp
+  bool canPartition(vector<int>& nums) {
         int n=nums.size();
         int sumii=accumulate(nums.begin(), nums.end(),0);
         if(sumii%2==1) return false;
@@ -39,4 +83,3 @@ bool  help(vector<int>&nums , int i,   int target, vector<vector<int>>&dp){
         }
          return  dp[0][target];
     }
-};
