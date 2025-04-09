bc2a47f624a568f2672c932956b6c3da46e72870
imankit1 <127870987+imankit1@users.noreply.github.com>
2025-04-09 22:36:58 +0530

    Update 0198-house-robber.cpp

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,3 +1,22 @@
+// recursive
+class Solution {
+public:
+    int helprob(vector<int>& nums, int idx){
+        //base case
+        if(idx>=nums.size()){
+            return 0;
+        }
+        int takehouse=nums[idx]+helprob(nums, idx+2);
+        int not_takehouse=helprob(nums, idx+1);
+
+        return max(takehouse, not_takehouse);
+    }
+    int rob(vector<int>& nums) {
+         
+        return helprob(nums, 0);
+    }
+};
+// memoization----------------------------------------------------------------------------------------------------------------------
 class Solution {
 public:
     int helprob(vector<int>& nums, int idx, vector<int>& dp){
@@ -13,9 +32,18 @@ public:
     }
     int rob(vector<int>& nums) {
         int n=nums.size();
-        //  vector<int>dp(n, -1);
-        // return helprob(nums, 0, dp);
+        return helprob(nums, 0, dp);
 
+    }
+};
+
+// tabulation bottom up 
+
+class Solution {
+public:
+    
+    int rob(vector<int>& nums) {
+        int n=nums.size();
         vector<int>dp(n+1, 0);
         dp[1]=nums[0];
         for(int i=2;i<=n;i++){
