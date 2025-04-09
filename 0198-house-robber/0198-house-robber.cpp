4217a0ac3320fa5c3b15d144d1a8c67848aadcc7
imankit1 <127870987+imankit1@users.noreply.github.com>
2025-04-09 22:33:14 +0530

    Time: 0 ms (100%), Space: 10.8 MB (22.04%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,23 +1,3 @@
-//recursive approach
-class Solution {
-public:
-    int helprob(vector<int>& nums, int idx){
-        //base case
-        if(idx>=nums.size()){
-            return 0;
-      
-        int takehouse=nums[idx]+helprob(nums, idx+2);
-        int not_takehouse=helprob(nums, idx+1);
-
-        return max(takehouse, not_takehouse);
-    }
-    int rob(vector<int>& nums) {
-         
-        return helprob(nums, 0);
-    }
-};
-// ------------------------------------------------------------------------------------------------------------------------------------------------
-//memoization
 class Solution {
 public:
     int helprob(vector<int>& nums, int idx, vector<int>& dp){
@@ -33,7 +13,16 @@ public:
     }
     int rob(vector<int>& nums) {
         int n=nums.size();
-         vector<int>dp(n, -1);
-        return helprob(nums, 0, dp);
+        //  vector<int>dp(n, -1);
+        // return helprob(nums, 0, dp);
+
+        vector<int>dp(n+1, 0);
+        dp[1]=nums[0];
+        for(int i=2;i<=n;i++){
+             int takehouse=nums[i-1]+dp[i-2];
+             int not_takehouse=dp[i-1];
+             dp[i]=max(takehouse, not_takehouse);
+        }
+        return dp[n];
     }
 };
