60068c5943b6f5cd7969fdaea2c16be61f32ee0d
imankit1 <127870987+imankit1@users.noreply.github.com>
2025-04-09 20:16:28 +0530

    Time: 0 ms (100%), Space: 10.6 MB (44.75%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int helprob(vector<int>& nums, int idx, vector<int>& dp){
+        //base case
+        if(idx>=nums.size()){
+            return 0;
+        }
+        if(dp[idx]!=-1) return dp[idx];
+        int takehouse=nums[idx]+helprob(nums, idx+2, dp);
+        int not_takehouse=helprob(nums, idx+1, dp);
+
+        return dp[idx]=max(takehouse, not_takehouse);
+    }
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+         vector<int>dp(n, -1);
+        return helprob(nums, 0, dp);
+    }
+};
