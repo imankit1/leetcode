225b97908bf9fc3649452051e0272a32b540be36
imankit1 <127870987+imankit1@users.noreply.github.com>
2025-04-24 11:08:51 +0530

    Time: 573 ms (22.36%), Space: 232.1 MB (22.09%) - LeetHub

--- 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.cpp
+++ 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.cpp
@@ -5,18 +5,19 @@ public:
         for(auto i: nums) st.insert(i);
 
         int sizeOfDinstinctElement=st.size();
-        st.clear();
+        // st.clear();
         int n=nums.size();
         int count=0;
 
 
         for(int i=0;i<=n-sizeOfDinstinctElement;i++){
+            unordered_set<int>st2;
             for(int j=i;j<n;j++){
-                st.insert(nums[j]);
-                if(st.size()==sizeOfDinstinctElement) count++;
-                if(st.size()>sizeOfDinstinctElement) break;
+                st2.insert(nums[j]);
+                if(st2.size()==sizeOfDinstinctElement) count++;
+                if(st2.size()>sizeOfDinstinctElement) break;
             }
-            st.clear();
+             
         }
         return count;
     }
