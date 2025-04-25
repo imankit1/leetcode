fd301446a799497d944a2a4969af9f3001e5b166
imankit1 <127870987+imankit1@users.noreply.github.com>
2025-04-25 22:56:26 +0530

    Time: 6 ms (41.96%), Space: 208.9 MB (32.72%) - LeetHub

---
+++ 3738-make-array-non-decreasing/3738-make-array-non-decreasing.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maximumPossibleSize(vector<int>& nums) {
+        int n=nums.size();
+
+        stack<int>st;
+        st.push(nums[0]);
+        for(int i=1;i<n;i++){
+            if(!st.empty() && st.top()<=nums[i]){
+                st.push(nums[i]);
+            }
+        }
+        return st.size();
+    }
+};
