488e2f33b7035b144b71920b67b8501d09a9d687
imankit1 <127870987+imankit1@users.noreply.github.com>
2025-05-02 09:58:08 +0530

    Time: 3 ms (97.89%), Space: 41.6 MB (60.42%) - LeetHub

--- 0853-most-profit-assigning-work/0853-most-profit-assigning-work.cpp
+++ 0853-most-profit-assigning-work/0853-most-profit-assigning-work.cpp
@@ -1,24 +1,45 @@
 class Solution {
 public:
+
+    int find(vector<pair<int, int>>&ds, int& target){
+        int lower=0;
+        int upper=ds.size()-1;
+        int ans=-1;
+        while(lower<=upper){
+            int mid=lower+(upper-lower)/2;
+
+            if(ds[mid].first<=target){
+                ans=mid;
+                lower=mid+1;
+            }
+            else{
+                upper=mid-1;
+            }
+        }
+        if(ans==-1) return 0;
+        return ds[ans].second;
+    }
     int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
         vector<pair<int, int>>ds;
 
         int n=profit.size();
 
         for(int i=0;i<n;i++){
-            ds.push_back(make_pair(profit[i], difficulty[i]));
+            ds.push_back(make_pair( difficulty[i], profit[i]));
         }
 
         sort(ds.begin(), ds.end());
 
-        long long maxProfit=0;
+        int maxii=0;
+        for(int i=0;i<n;i++){
+           maxii=max(maxii, ds[i].second);
+           ds[i].second=maxii;
+        }
+          int maxProfit=0;
         for(int i=0;i<worker.size();i++){
-            for(int j=ds.size()-1;j>=0;j--){
-                if(worker[i]>=ds[j].second){
-                    maxProfit+=ds[j].first;
-                    break;
-                }
-            }
+               int target=worker[i];
+               int ans=find(ds, target);
+             maxProfit+=ans;
         }
         return maxProfit;
     }
