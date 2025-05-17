3be619ed8706109d4334a5515a73c1b32802a8d3
imankit1 <127870987+imankit1@users.noreply.github.com>
2025-05-17 16:10:24 +0530

    Time: 0 ms (100%), Space: 11.7 MB (48.91%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,42 +1,24 @@
 class Solution {
 public:
-void mergeSort(vector<int>& nums, int s, int mid, int e){
-    vector<int>temp;
-    int l=s, h=mid+1;
-    while(l<=mid && h<=e){
-        if(nums[l]<=nums[h]){
-            temp.push_back(nums[l]);
-            l++;
-        }
-        else{
-            temp.push_back(nums[h]);
-            h++;
-        }
-    }
-    while(l<=mid){
-        temp.push_back(nums[l]);
-        l++;
-    }
-    while(h<=e){
-        temp.push_back(nums[h]);
-        h++;
-    }
-
-    for(int i=s;i<=e;i++){
-        nums[i]=temp[i-s];
-    }
-}
-void merge(vector<int>& nums, int s, int e){
-    if(s>=e){
-        return;
-    }
-    int mid=s+(e-s)/2;
-    merge(nums, s, mid);
-    merge(nums, mid+1, e);
-    mergeSort(nums, s, mid, e);
-}
     void sortColors(vector<int>& nums) {
-        int n=nums.size()-1;
-        merge(nums, 0, n);
+        int ones=0, zeros=0, twos=0;
+        for(auto i:nums){
+            if(i==0) zeros++;
+            else if(i==1) ones++;
+            else twos++;
+        }
+        int i=0;
+        while(zeros--){
+            nums[i]=0;
+            i++;
+        }
+        while(ones--){
+            nums[i]=1;
+            i++;
+        }
+        while(twos--){
+            nums[i]=2;
+            i++;
+        }
     }
 };
