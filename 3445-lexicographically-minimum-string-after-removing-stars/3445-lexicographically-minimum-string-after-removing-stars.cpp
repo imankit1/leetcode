210a387d55fc7f13f7ccaf35d17dcc101f3cee96
imankit1 <127870987+imankit1@users.noreply.github.com>
2025-06-07 21:24:36 +0530

    Time: 142 ms (50.65%), Space: 26 MB (73.7%) - LeetHub

---
+++ 3445-lexicographically-minimum-string-after-removing-stars/3445-lexicographically-minimum-string-after-removing-stars.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+typedef pair<char, int>P;
+
+    struct comp{
+
+        bool operator()(P &p1, P&p2){
+            if(p1.first==p2.first) return p1.second<p2.second;
+            return p1.first>p2.first;
+        }
+
+    };
+    string clearStars(string s) {
+        //priority queue
+
+        priority_queue<P, vector<P>, comp>pq;
+         
+
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='*') {
+                int idx=pq.top().second;
+                pq.pop();
+                s[idx]='*';
+            }
+            else pq.push({s[i], i});
+        }
+
+        
+        string ans="";
+        for(int i=0;i<s.size();i++){
+             if(s[i]!='*') ans+=s[i];
+        }
+        return ans;
+    }
+};
