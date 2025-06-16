f802d0f7c3a8178aadd75cb1ce546f4ccbb8e54c
imankit1 <127870987+imankit1@users.noreply.github.com>
2025-06-16 11:52:23 +0530

    Time: 5 ms (11.76%), Space: 12.6 MB (5.67%) - LeetHub

---
+++ 2144-maximum-difference-between-increasing-elements/2144-maximum-difference-between-increasing-elements.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int fun(vector<int>& nums, vector<int>& temp){
+        int maxii=-1;
+        for(int i=0;i<nums.size();i++){
+            maxii=max(maxii, temp[i]-nums[i]);
+        }
+        return maxii==0 ? -1 : maxii;
+    }
+    int f(vector<int>& nums){
+        int maxii=-1;
+        int n=nums.size();
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                if(nums[i]<nums[j])
+                maxii=max(maxii, nums[j]-nums[i]);
+            }
+        }
+        return maxii;
+    }
+    int maximumDifference(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>temp(n);
+        int maxii=nums[n-1];
+        for(int i=n-1;i>=0;i--){
+            maxii=max(maxii, nums[i]);
+            temp[i]=maxii;
+        }
+         return f(nums);
+        //  return fun(nums, temp);
+    }
+};
