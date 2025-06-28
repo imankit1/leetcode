50ce7aad2b204d99e9ce988c399a737c25e4cb71
imankit1 <127870987+imankit1@users.noreply.github.com>
2025-06-28 16:34:12 +0530

    Time: 0 ms (100%), Space: 21.7 MB (70.25%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int sum=0;
+        for(auto i:nums) sum+=i;
+        int n=nums.size();
+        int totalSum=n*(n+1)/2;
+        return totalSum-sum;
+    }
+};
