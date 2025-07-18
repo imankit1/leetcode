ca168817c3d8ff7ade25875096178031829c0061
imankit1 <127870987+imankit1@users.noreply.github.com>
2025-07-18 21:08:48 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,52 +1,20 @@
-
-// recursive method
 class Solution {
 public:
-    void check(vector<int>& nums, vector<int>& temp, int prev, int& maxii, int i){
-        if(i>=nums.size()){
-            if(maxii<temp.size()){
-                maxii=temp.size();
-            }
-                return;
+
+    int solve(vector<int>& nums, int idx, int prev){
+        if(idx>=nums.size()){
+            return 0;
+        }
+        int take=0;
+        if(prev==-1 || nums[prev]<nums[idx]){
+            take=1+solve(nums, idx+1, idx);
         }
 
-            if(prev<nums[i]){
-                temp.push_back(nums[i]);
-                check(nums, temp, nums[i], maxii, i+1);
-                temp.pop_back();
-            }
-            check(nums, temp, prev, maxii, i+1);
-        }
+        int skip=solve(nums, idx+1, prev);
+
+        return max(take, skip);
+    }
     int lengthOfLIS(vector<int>& nums) {
-        vector<int>temp;
-        int prev=INT_MIN;
-        int maxii=1;
-        check(nums, temp, prev, maxii, 0);
-        return maxii;
+        return solve(nums, 0, -1);
     }
 };
-//  tabulation method
-
-class Solution {
-public:
-    int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        int maxii = 1;
-        vector<int> dp(n + 1, 1);
-        for (int i = 1; i < n; i++) {
-
-            for (int j = 0; j < i; j++) {
-                if (nums[j] < nums[i]) {
-                    if (dp[i] < dp[j] + 1) {
-                        dp[i] = dp[j] + 1;
-                    }
-                    if (maxii < dp[i]) {
-                        maxii = dp[i];
-                        
-                    }
-                }
-            }
-        }
-            return maxii;
-        }
-    };
