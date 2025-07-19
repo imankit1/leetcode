240f07e15d05e42afd98c14a77a92c7df9939faf
imankit1 <127870987+imankit1@users.noreply.github.com>
2025-07-19 13:22:03 +0530

    Time: 126 ms (32.47%), Space: 47.3 MB (87.96%) - LeetHub

---
+++ 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<string> removeSubfolders(vector<string>& folder) {
+   int n=folder.size();
+   sort(folder.begin(), folder.end());
+    vector<string>ans;
+
+    string temp=folder[0];
+    ans.push_back(temp);
+    for(int i=1;i<n;i++){
+        string curr=folder[i].substr(0, temp.size());
+
+        if(temp==curr && folder[i][temp.size()]=='/'){
+            cout<<"flag";
+            continue;
+        }
+        else{
+         temp=folder[i];
+         ans.push_back(folder[i]);
+
+        } 
+    }
+    return ans;
+    }
+};
