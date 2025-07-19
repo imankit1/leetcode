8bd537c9dca0f40bf2f9f1876c49449a0a4d9402
imankit1 <127870987+imankit1@users.noreply.github.com>
2025-07-19 10:33:23 +0530

    Time: 30 ms (46.17%), Space: 11.2 MB (31.77%) - LeetHub

---
+++ 1457-minimum-difficulty-of-a-job-schedule/1457-minimum-difficulty-of-a-job-schedule.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int solve(vector<int>& jd, int n, int idx, int d, vector<vector<int>>&dp){
+        if(d==1){
+            int maxii=jd[idx];
+
+            for(int i=idx; i<n;i++){
+                maxii=max(maxii, jd[i]);
+            }
+
+
+            return maxii;
+        }
+        if(dp[idx][d]!=-1) return dp[idx][d];
+        int maxii= jd[idx];
+
+        int finalResult=INT_MAX;
+
+        for(int i=idx;i<=n-d;i++){
+            maxii=max(maxii, jd[i]);
+
+            int result=maxii+solve(jd, n, i+1, d-1, dp);
+
+            finalResult=min(finalResult, result);
+        }
+
+        return dp[idx][d]=finalResult;
+    }
+    int minDifficulty(vector<int>& jd, int d) {
+        int n=jd.size();
+        if(n<d) return -1;
+
+        vector<vector<int>>dp(n+1, vector<int>(d+1,-1));
+        return solve(jd, n, 0, d, dp);
+    }
+};
