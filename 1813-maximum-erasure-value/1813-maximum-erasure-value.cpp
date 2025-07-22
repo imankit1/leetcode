de0e3be3211800243506f8006230738547c08ece
imankit1 <127870987+imankit1@users.noreply.github.com>
2025-07-22 12:47:13 +0530

    Time: 221 ms (15.59%), Space: 131 MB (31.9%) - LeetHub

---
+++ 1813-maximum-erasure-value/1813-maximum-erasure-value.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maximumUniqueSubarray(vector<int>& nums) {
+        unordered_map<int, int>mpp;
+        int sum=0;
+        int ans=0;
+        int l=0;
+        for(int i=0;i<nums.size();i++){
+            while(mpp.find(nums[i])!=mpp.end()){
+                mpp[nums[l]]--;
+                if(mpp[nums[l]]==0) mpp.erase(nums[l]);
+                sum-=nums[l];
+                l++;
+            }
+            mpp[nums[i]]++;
+            sum+=nums[i];
+            ans=max(ans, sum);
+        }
+        return ans;
+    }
+};
