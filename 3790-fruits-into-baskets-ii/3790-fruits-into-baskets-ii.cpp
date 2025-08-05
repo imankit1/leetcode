668103b8ad2f08291ead33f87a63b4ebc039c4bc
imankit1 <127870987+imankit1@users.noreply.github.com>
2025-08-05 22:50:23 +0530

    Time: 0 ms (100%), Space: 48.8 MB (75.84%) - LeetHub

--- 3790-fruits-into-baskets-ii/3790-fruits-into-baskets-ii.cpp
+++ 3790-fruits-into-baskets-ii/3790-fruits-into-baskets-ii.cpp
@@ -3,20 +3,21 @@ public:
     int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
         int n=baskets.size();
         vector<bool>visited(n, false);
+        int cnt=0;
         for(int i=0;i<n;i++){
+            bool flag=false;
+             
             for(int j=0;j<n;j++){
                 if(fruits[i]<=baskets[j] && !visited[j]){
                     visited[j]=true;
+                    flag=true;
+                     
                     break;
                 }
             }
+            if(flag==false) cnt++;
         }
-        int cnt=0;
-        for(int i=0;i<n;i++){
-            if(visited[i]==false){
-                cnt++;
-            }
-        }
+         
         return cnt;
     }
 };
