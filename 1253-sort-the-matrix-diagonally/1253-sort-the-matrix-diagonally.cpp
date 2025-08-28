e2cb312a604ada2b2c29d32269e02934318deeb4
imankit1 <127870987+imankit1@users.noreply.github.com>
2025-08-28 14:22:06 +0530

    Time: 0 ms (100%), Space: 13.6 MB (65.5%) - LeetHub

--- 1253-sort-the-matrix-diagonally/1253-sort-the-matrix-diagonally.cpp
+++ 1253-sort-the-matrix-diagonally/1253-sort-the-matrix-diagonally.cpp
@@ -1,47 +1,57 @@
 class Solution {
 public:
     vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
-        int r = mat.size();
-        int c = mat[0].size();
+        int r=mat.size();
+        int c=mat[0].size();
+        
+        vector<vector<int>>ans(r, vector<int>(c));
+
+        for(int i=0;i<r;i++){
+            int j=i;
+            int k=0;
+            vector<int>temp;
+            while(j<r && k<c){
+                temp.push_back(mat[j][k]);
+                j++;
+                k++;
+            }
+            sort(temp.begin(), temp.end());
+            j=i;
+            k=0;
+            int u=0;
+              while(j<r && k<c){
+                ans[j][k]=temp[u];
+                j++;
+                k++;
+                u++;
+            }
+        }
+       
+        
+       for(int i=1; i<c; i++){   
+    int j=0, k=i;
+    vector<int> temp1;
+
+    
+    while(j<r && k<c){
+        temp1.push_back(mat[j][k]);
+        j++;
+        k++;
+    }
 
      
-        for(int i = 0; i < r; i++){
-            int j = i, k = 0;
-            vector<int> temp;
-            while(j < r && k < c){
-                temp.push_back(mat[j][k]);
-                j++;
-                k++;
-            }
-            sort(temp.begin(), temp.end());
-            j = i; k = 0;
-            int u = 0;
-            while(j < r && k < c){
-                mat[j][k] = temp[u++];
-                j++;
-                k++;
-            }
-        }
+    sort(temp1.begin(), temp1.end());
+ 
+    j=0; k=i;
+    int u=0;
+    while(j<r && k<c){
+        ans[j][k] = temp1[u];
+        j++;
+        k++;
+        u++;
+    }
+}
 
-   
-        for(int i = 1; i < c; i++){
-            int j = 0, k = i;
-            vector<int> temp;
-            while(j < r && k < c){
-                temp.push_back(mat[j][k]);
-                j++;
-                k++;
-            }
-            sort(temp.begin(), temp.end());
-            j = 0; k = i;
-            int u = 0;
-            while(j < r && k < c){
-                mat[j][k] = temp[u++];
-                j++;
-                k++;
-            }
-        }
-
-        return mat;
+        return ans;
     }
 };
