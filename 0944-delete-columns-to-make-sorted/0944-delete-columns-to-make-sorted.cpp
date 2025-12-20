cbe2bf3864ea3c30bb8989df1cb2e205be4b55a4
imankit1 <127870987+imankit1@users.noreply.github.com>
2025-12-20 20:46:06 +0530

    Time: 3 ms (63.02%), Space: 15.6 MB (14.42%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
@@ -1,40 +1,24 @@
 class Solution {
 public:
     int minDeletionSize(vector<string>& strs) {
-        vector<vector<char>>ch;
-        int n=strs.size();
-
-        int c=INT_MAX;
-        for(int i=0;i<n;i++){
-            string s=strs[i];
-            vector<char>temp;
-            for(int j=0;j<s.size();j++){
-             temp.push_back(s[j]);
-            }
-
-            c=min(c, (int)temp.size());
-            ch.push_back(temp);
-        }
+        
+        int row=strs.size();
+        int col=strs[0].size();
 
 
         int count=0;
-       
-        for (int i = 0; i < c; i++) {
-             char d=ch[0][i];
-            for (int j = 0; j < n; j++) {
-                if (d - 'a' <= ch[j][i] - 'a')  {
 
-                    d=ch[j][i];
-                }
-                else {
-                    count++;
-                    break;
-                }
-                    
+        for(int i=0;i<col;i++){
+            for(int j=1;j<row;j++){
+                if(strs[j][i]<strs[j-1][i]){
+                     count++;
+                     break;
+                } 
             }
-            
         }
-   
+
         return count;
+
+
     }
 };
