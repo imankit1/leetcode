bd303603488c6d972c3ae08e138f880ea42448dc
imankit1 <127870987+imankit1@users.noreply.github.com>
2026-02-17 20:19:57 +0530

    Time: 0 ms (100%), Space: 9.8 MB (41.32%) - LeetHub

---
+++ 0401-binary-watch/0401-binary-watch.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<string> readBinaryWatch(int turnedOn) {
+
+        vector<string>result;
+        for(int i=0;i<=11;i++){  //for hours
+                for(int j=0;j<=59;j++){ //for minutes
+
+                    if(__builtin_popcount(i)+__builtin_popcount(j)==turnedOn){
+                        string h=to_string(i);
+                        string m=(j<10 ? "0" : "")+ to_string(j);
+                        result.push_back(h+":"+m);
+                    }
+
+                }
+        }
+        return result;
+    }
+};
