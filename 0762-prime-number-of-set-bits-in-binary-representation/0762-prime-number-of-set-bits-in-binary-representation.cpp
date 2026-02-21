e4a8e7a4be0cbeb6477fa79f7680f3451d579de4
imankit1 <127870987+imankit1@users.noreply.github.com>
2026-02-21 11:20:51 +0530

    Time: 3 ms (72.17%), Space: 7.9 MB (61.81%) - LeetHub

--- 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.cpp
+++ 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     bool checkPrime(int n){
         if (n==1) return false;
-        if (n==2) return true;
-        for(int i=2;i<n;i++){
+        for(int i=2;i<=sqrt(n);i++){
             if(n%i==0) return false;
         }
         return true;
