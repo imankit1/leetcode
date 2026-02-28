5ee79a41d94e365af885a52beadf6a85029ac76a
imankit1 <127870987+imankit1@users.noreply.github.com>
2026-02-28 12:42:02 +0530

    Time: 48 ms (53.02%), Space: 7.9 MB (99.33%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
@@ -2,10 +2,11 @@ class Solution {
 public:
     int MOD=1000000007;
     int concatenatedBinary(int n) {
+        long long bit=0;
         long long result=0;
         for(int num=1;num<=n;num++){
-            int digit=log2(num)+1; // kitne bit ka space chahiye.
-            result=((result<<digit) %MOD + num)%MOD;
+            if((num&(num-1))==0) bit++;
+            result=((result<<bit) %MOD + num)%MOD;
         }
         return result;
     }
