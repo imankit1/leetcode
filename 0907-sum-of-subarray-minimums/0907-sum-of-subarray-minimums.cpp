44dff7e0c3976436efaef041349d5a0730a93149
imankit1 <127870987+imankit1@users.noreply.github.com>
2026-03-12 19:01:44 +0530

    Time: 18 ms (43.13%), Space: 47.1 MB (58.7%) - LeetHub

---
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    int mod=1e9+7;
+
+    vector<int>pSE(vector<int>& arr){
+        int n=arr.size();
+        stack<int>st;
+        vector<int>result(n);
+        for(int i=0;i<n;i++){
+            while(!st.empty() && arr[st.top()]>arr[i]) st.pop();
+            
+            result[i]=st.empty() ? -1: st.top();
+            st.push(i);
+        }
+        
+        return result;
+    }
+vector<int>nSE(vector<int>& arr){
+      stack<int>st;
+      int n=arr.size();
+      vector<int>result(n);
+      for(int i=n-1;i>=0;i--){
+          while(!st.empty() && arr[st.top()]>=arr[i]) st.pop();
+          
+          result[i]=st.empty()? n :st.top();
+          
+          st.push(i);
+          
+      }
+      
+      return result;
+  }
+    int sumSubarrayMins(vector<int>& arr) {
+
+        vector<int>nse=nSE(arr);
+        vector<int>pse=pSE(arr);
+        int n=arr.size();
+
+        long long result=0;
+        for(int i=0;i<n;i++){
+            int left=i-nse[i];
+            int right=pse[i]-i;
+
+          
+
+            long long total = (left * right) % mod;
+            total = (total * arr[i]) % mod;
+
+            result = (result + total) % mod;
+        }
+        return result;
+        
+    }
+};
