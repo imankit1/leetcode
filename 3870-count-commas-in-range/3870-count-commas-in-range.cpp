83e00eaba24a5e83de405f2f2f6bb92469dc14d6
imankit1 <127870987+imankit1@users.noreply.github.com>
2026-03-15 09:46:49 +0530

    Time: 0 ms (100%), Space: 8.4 MB (86.82%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -1,13 +1,7 @@
 class Solution {
 public:
     int countCommas(int n) {
-        int comma=0;
-        if(n==100000) return 99001;
-        for(int i=1000;i<=n;i++){
-            string s=to_string(i);
-            int n=s.size();
-            comma+=n/3;
-        }
-        return comma;
+        if(n<1000) return 0;
+        return n-999;
     }
 };
