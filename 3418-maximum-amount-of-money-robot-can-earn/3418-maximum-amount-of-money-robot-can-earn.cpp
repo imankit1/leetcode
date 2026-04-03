4d029adcc3e033da05af8d5186f3d9d47c26c21e
imankit1 <127870987+imankit1@users.noreply.github.com>
2026-04-03 21:53:06 +0530

    Update 3418-maximum-amount-of-money-robot-can-earn.cpp

--- 3418-maximum-amount-of-money-robot-can-earn/3418-maximum-amount-of-money-robot-can-earn.cpp
+++ 3418-maximum-amount-of-money-robot-can-earn/3418-maximum-amount-of-money-robot-can-earn.cpp
@@ -3,6 +3,47 @@ public:
     int m;  //row size
     int n;  // column size
 
+
+    int solve(vector<vector<int>>& coins, int i, int j, int neu){
+        // at m-1, n-1
+        if(i==m-1 && j==n-1){
+            if(coins[i][j]<0 && neu>0) return 0;
+            return coins[i][j];
+        }
+        if(i>=m || j>=n){
+            return INT_MIN;
+        }
+
+        //take
+        int take=coins[i][j]+max(solve(coins, i+1, j, neu), solve(coins, i, j+1, neu));
+
+        //skip
+        int notTake=INT_MIN;
+        if(coins[i][j]<0 && neu>0){
+            int skipDown=solve(coins, i+1, j, neu-1);
+            int skipRight=solve(coins, i, j+1, neu-1);
+
+            notTake=max(skipDown, skipRight);
+        }
+        return max(take, notTake);
+    }
+    int maximumAmount(vector<vector<int>>& coins) {
+        m=coins.size();
+        n=coins[0].size();
+        return solve(coins, 0, 0, 2);
+    }
+};
+
+
+
+// _________________________________________________________dp
+
+
+class Solution {
+public:
+    int m;  //row size
+    int n;  // column size
+
     int t[501][501][3];
     int solve(vector<vector<int>>& coins, int i, int j, int neu){
         // at m-1, n-1
