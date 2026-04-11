3605db4ac9e41e8621fcb6440f2b55b45b1dc600
imankit1 <127870987+imankit1@users.noreply.github.com>
2026-04-11 12:20:40 +0530

    Time: 836 ms (39.83%), Space: 179.1 MB (90.76%) - LeetHub

---
+++ 3890-integers-with-multiple-sum-of-two-cubes/3890-integers-with-multiple-sum-of-two-cubes.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> findGoodIntegers(int n) {
+
+        unordered_map<int, int>mp;
+        for(int i=1;i<=cbrt(n);i++){
+            for(int j=i+1;j<=cbrt(n);j++){
+                long long cube=pow(i,3)+pow(j,3);
+                if(cube>n) break;
+                mp[cube]++;
+            }
+        }
+        vector<int>res;
+        for(auto &i:mp){
+            if(i.second>1) res.push_back(i.first);
+        }
+        sort(res.begin(), res.end());
+        return res;
+    }
+};
