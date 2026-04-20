b37b5488789248d3df7f3a98396109a4be0ab983
imankit1 <127870987+imankit1@users.noreply.github.com>
2026-04-20 10:48:34 +0530

    Time: 2 ms (43.58%), Space: 43.4 MB (27.78%) - LeetHub

--- 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.java
+++ 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.java
@@ -7,7 +7,7 @@ class Solution {
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
                 if(colors[i]!=colors[j]){
-                    res=Math.max(res, Math.abs(j-i));
+                    res=Math.max(res,(j-i));
                 }
             }
         }
