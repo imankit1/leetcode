e0b8156f03926b8f09f5b0cb0ca870e988f050b9
imankit1 <127870987+imankit1@users.noreply.github.com>
2026-04-28 11:23:49 +0530

    Time: 8 ms (69.28%), Space: 46.1 MB (64.76%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,32 +1,3 @@
-// Brute force
-
-class Solution {
-  public:
-    int longestSubstr(string& s, int k) {
-        // code here
-        int n=s.size();
-        int maxii=INT_MIN; 
-        for(int i=0;i<n;i++){
-            int cnt=0;
-            for(int j=i+1;j<n;j++){
-                if(s[i]!=s[j]){
-                    cnt++;
-                }
-                if(cnt>k){
-                    break;
-                }
-                maxii=max(maxii, j-i+1);
-            }
-        }
-        return maxii;
-    }
-};
-
-// ______________________________________________________________________
-//     Optimize approach 
-
-
-
 class Solution {
     public int characterReplacement(String s, int k) {
         int r=0, l=0, maxf=0, maxl=0;
@@ -36,11 +7,9 @@ class Solution {
             maxf=Math.max(maxf, hash[s.charAt(r)-'A']);
             
             while((r-l+1)-maxf > k){
-                maxf=0;
+
                 hash[s.charAt(l)-'A']--;
-                for(int i=0;i<26;i++){
-                    maxf=Math.max(maxf, hash[i]);
-                }
+                
                 l=l+1;
             }
             if((r-l+1)-maxf<=k){
