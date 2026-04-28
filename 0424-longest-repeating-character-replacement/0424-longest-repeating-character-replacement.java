e7c62762390d3698a2517e052488c189200c1042
imankit1 <127870987+imankit1@users.noreply.github.com>
2026-04-28 11:12:46 +0530

    Time: 8 ms (69.28%), Space: 46.1 MB (78.91%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int r=0, l=0, maxf=0, maxl=0;
+        int hash[]=new int [26];
+        while(r<s.length()){
+            hash[s.charAt(r)-'A']++;
+            maxf=Math.max(maxf, hash[s.charAt(r)-'A']);
+            
+            while((r-l+1)-maxf > k){
+                maxf=0;
+                hash[s.charAt(l)-'A']--;
+                for(int i=0;i<26;i++){
+                    maxf=Math.max(maxf, hash[i]);
+                }
+                l=l+1;
+            }
+            if((r-l+1)-maxf<=k){
+                maxl=Math.max(maxl, r-l+1);
+            }
+            r++;
+        }
+        return maxl;
+    }
+}
