4709651de79f911b6b3433fa2664846bc3b35c13
imankit1 <127870987+imankit1@users.noreply.github.com>
2026-04-29 11:30:07 +0530

    Time: 47 ms (66.7%), Space: 29.2 MB (28.04%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,33 +1,25 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-         vector<vector<int>>ans;
-        
         sort(nums.begin(), nums.end());
+        vector<vector<int>>res;
         int n=nums.size();
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n-1;i++){
             if(i>0 && nums[i]==nums[i-1]) continue;
             int j=i+1;
             int k=n-1;
-
             while(j<k){
-                int sum=nums[i]+nums[j]+nums[k];
-                if(sum<0){
-                    j++;
-                }
-                else if(sum>0){
-                    k--;
-                }
-                else {
-                  
-                    ans.push_back({nums[i], nums[j], nums[k]});
+                if(nums[i]+nums[j]+nums[k]==0){
+                    res.push_back({nums[i], nums[j], nums[k]});
                     j++;
                     k--;
-                while( j<k && nums[j]==nums[j-1]) j++;
-                while(j<k && nums[k]==nums[k+1]) k--;
+                    while(j<k && nums[j]==nums[j-1])j++;
+                    while(j<k && nums[k]==nums[k+1]) k--;
                 }
+                else if(nums[i]+nums[j]+nums[k]<0) j++;
+                else k--;
             }
         }
-        return ans;
+        return res;
     }
 };
