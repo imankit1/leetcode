fc98aaa9f06c19e28c68309178be856ed5a77f8c
imankit1 <127870987+imankit1@users.noreply.github.com>
2026-05-03 16:42:42 +0530

    Time: 8 ms (19.53%), Space: 46 MB (5.09%) - LeetHub

---
+++ 3918-sum-of-primes-between-number-and-its-reverse/3918-sum-of-primes-between-number-and-its-reverse.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[] primeNumber(int n){
+        int[] prime= new int[1001];
+
+        for(int i=2;i*i<=1000;i++){
+            if(prime[i]==1) continue;
+            for(int j=i*i;j<=1000;j+=i){
+                prime[j]=1;
+            }
+        }
+        prime[0]=1;
+        prime[1]=1;
+        return prime;
+    }
+    public int revSolve(int n){
+        int num=0;
+        while(n>0){
+            num=num*10+(n%10);
+            n/=10;
+        }
+        return num;
+    }
+    public int sumOfPrimesInRange(int n) {
+        int[] prime= primeNumber(n);
+        int rev=revSolve(n);
+        int res=0;
+        System.out.println(rev);
+        for(int i=Math.min(n, rev); i<=Math.max(n, rev); i++){
+             
+            if(prime[i]==0) res+=i;
+        }
+        return res;
+    }
+}
