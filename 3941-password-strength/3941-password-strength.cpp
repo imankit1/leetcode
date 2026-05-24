f1ba0d196164ddcdf4a4b6c0fad224f5baebfe71
imankit1 <127870987+imankit1@users.noreply.github.com>
2026-05-24 12:31:11 +0530

    Time: 27 ms (29.23%), Space: 17.6 MB (50.7%) - LeetHub

---
+++ 3941-password-strength/3941-password-strength.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int passwordStrength(string p) {
+        
+        unordered_map<char, int>mp;
+        int res = 0;
+
+        for(int i = 0;i< p.size(); i++){
+            if(p[i] >= 'a' && p[i] <= 'z' && mp[p[i]] == 0){
+                res += 1;
+                mp[p[i]] = 1;
+            }
+              else if(p[i] >= 'A' && p[i] <= 'Z' && mp[p[i]] == 0){
+                res += 2;
+                mp[p[i]] = 1;
+            }
+             else if(p[i] >= '0' && p[i] <= '9' && mp[p[i]] == 0){
+                res += 3;
+                mp[p[i]] = 1;
+            }
+
+            else if(mp[p[i]] == 0){
+                res += 5;
+                mp[p[i]] = 1;
+            }
+            
+        }
+        return res;
+    }
+};
