868f1f93e1536cdd0aa3e885f0e855cd30b7c0e8
imankit1 <127870987+imankit1@users.noreply.github.com>
2026-06-22 10:03:31 +0530

    Time: 595 ms (40.38%), Space: 0B (100%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -2,8 +2,8 @@
 
 
 select 
-distinct l1.num as ConsecutiveNums
+l1.num as ConsecutiveNums
 
 from logs l1, logs l2, logs l3
 where l1.id = l2.id-1 and l2.id = l3.id -1 and l2.num = l1.num and l3.num = l2.num
--- group by l1.num
+group by l1.num
