2e478e4f34de8050314baeecca8928fe3f141c1d
imankit1 <127870987+imankit1@users.noreply.github.com>
2026-06-28 13:26:16 +0530

    Time: 49 ms (69.33%), Space: 149 MB (33.47%) - LeetHub

--- 3974-maximum-total-sum-of-k-selected-elements/3974-maximum-total-sum-of-k-selected-elements.java
+++ 3974-maximum-total-sum-of-k-selected-elements/3974-maximum-total-sum-of-k-selected-elements.java
@@ -5,7 +5,7 @@ class Solution {
         long res = 0;
         Arrays.sort(nums);
         for(int i = n-1;i >= (n-k); i--){
-            int num = nums[i];
+            long num = nums[i];
             res += num*mul;
             mul--;
             if(mul == 0) mul = 1;
