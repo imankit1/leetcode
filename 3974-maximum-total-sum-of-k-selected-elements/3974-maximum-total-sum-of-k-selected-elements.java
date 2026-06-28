9fa7431fbadaafdd6c2d15500b03954afa95b389
imankit1 <127870987+imankit1@users.noreply.github.com>
2026-06-28 13:26:12 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3974-maximum-total-sum-of-k-selected-elements/3974-maximum-total-sum-of-k-selected-elements.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long maxSum(int[] nums, int k, int mul) {
+        
+        int n = nums.length;
+        long res = 0;
+        Arrays.sort(nums);
+        for(int i = n-1;i >= (n-k); i--){
+            int num = nums[i];
+            res += num*mul;
+            mul--;
+            if(mul == 0) mul = 1;
+        }
+        return res;
+    }
+}
