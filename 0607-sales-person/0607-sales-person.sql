42578233967e11d793062f02f57cbd5eec06e47c
imankit1 <127870987+imankit1@users.noreply.github.com>
2026-06-28 16:36:32 +0530

    Time: 1456 ms (55.29%), Space: 0B (100%) - LeetHub

---
+++ 0607-sales-person/0607-sales-person.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+
+select s.name from salesPerson s 
+where s.name not in (
+select 
+s.name
+from salesperson s left join orders  o on s.sales_id = o.sales_id
+left join company c on c.com_id = o.com_id
+where c.name = 'RED')
+ 
