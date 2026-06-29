e850d9dd5819c1f766432ce50aa89405305d90ca
imankit1 <127870987+imankit1@users.noreply.github.com>
2026-06-29 11:05:52 +0530

    Time: 50 ms (5.34%), Space: 33.6 MB (5.1%) - LeetHub

---
+++ 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numOfStrings(vector<string>& patterns, string word) {
+        unordered_map<string, int>mp;
+        for(int i = 0; i < word.size(); i++){
+            string temp = "";
+            for(int j = i; j < word.size(); j++){
+                temp.push_back(word[j]);
+                mp[temp]++;
+            }
+        }
+        int cnt = 0;
+        for(auto &j:patterns){
+            if(mp.find(j) != mp.end()) cnt++;
+        }
+
+        return cnt;
+    }
+};
