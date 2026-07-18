c13f62a2d2ee6de6e288c99da415632bacebb195
imankit1 <127870987+imankit1@users.noreply.github.com>
2026-07-18 16:51:34 +0530

    Time: 0 ms (100%), Space: 18.6 MB (14.65%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -7,23 +7,23 @@ public:
         
         vector<vector<int>>visited(n, vector<int>(m ,0));
         
-        vector<vector<int>>temp(n, vector<int>(m ,0));
+        // vector<vector<int>>temp(n, vector<int>(m ,0));
         
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                temp[i][j] = image[i][j];
-            }
-        }
+        // for(int i = 0; i < n; i++){
+        //     for(int j = 0; j < m; j++){
+        //         temp[i][j] = image[i][j];
+        //     }
+        // }
         
         queue<pair<int, int>>q;
+        int initialColor = image[sr][sc];
         
         q.push({sr, sc});
         visited[sr][sc] = 1;
-        temp[sr][sc] = color;
+        image[sr][sc] = color;
         while(!q.empty()){
             int r = q.front().first;
             int c = q.front().second;
-            
             q.pop();
             
             
@@ -34,14 +34,14 @@ public:
                 int nr = r + nrow[i];
                 int nc = c + ncol[i];
             if(nr >= 0 && nr < n && nc >=0 && nc < m && !visited[nr][nc] &&
-            image[nr][nc] == image[r][c]){
+            image[nr][nc] == initialColor){
                 visited[nr][nc] = 1;
                 q.push({nr, nc});
-                temp[nr][nc] = color;
+                image[nr][nc] = color;
             }
             }
         }
         
-        return temp;
+        return image;
     }
 };
