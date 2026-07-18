8988f5c38b74506f94a93b1586d17ad61d1798ea
imankit1 <127870987+imankit1@users.noreply.github.com>
2026-07-18 16:48:19 +0530

    Time: 3 ms (12.82%), Space: 18.6 MB (8.5%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+          int n = image.size();   // row
+        int m = image[0].size();  //col
+        
+        
+        vector<vector<int>>visited(n, vector<int>(m ,0));
+        
+        vector<vector<int>>temp(n, vector<int>(m ,0));
+        
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                temp[i][j] = image[i][j];
+            }
+        }
+        
+        queue<pair<int, int>>q;
+        
+        q.push({sr, sc});
+        visited[sr][sc] = 1;
+        temp[sr][sc] = color;
+        while(!q.empty()){
+            int r = q.front().first;
+            int c = q.front().second;
+            
+            q.pop();
+            
+            
+            int nrow[] = {0, -1, 0, 1};
+            int ncol[] = {-1, 0, 1, 0};
+            
+            for(int i = 0; i < 4; i++){
+                int nr = r + nrow[i];
+                int nc = c + ncol[i];
+            if(nr >= 0 && nr < n && nc >=0 && nc < m && !visited[nr][nc] &&
+            image[nr][nc] == image[r][c]){
+                visited[nr][nc] = 1;
+                q.push({nr, nc});
+                temp[nr][nc] = color;
+            }
+            }
+        }
+        
+        return temp;
+    }
+};
